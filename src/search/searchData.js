aa327a333a8077e2a9adb5e56806c2a1003e4a1a
mahavishnu <mahanvishnu3@gamil.com>
2025-11-17 01:46:29 +0530

    update

--- src/search/searchData.js
+++ src/search/searchData.js
@@ -1,3 +1,5 @@
+
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyAm--sfbEonD49xNHSWPWoeMbkwFXXqF4U",
@@ -12,14 +14,25 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 
+window.addEventListener('load', () => {
+    const loaderContent = document.getElementsByClassName('dots-container')[0];
+    loaderContent.classList.add('loader-hidden')
+})
 // first
 
 db.ref("popularinstamart/").get('value').then((snapshot) => {
     const data = snapshot.val()
+  
+
+
     const mainData = document.getElementsByClassName('img-data')[0];
     const div = document.createElement('div');
     div.classList.add('img-data-div');
     data.forEach((item) => {
+
+
+
+
         div.innerHTML += `
             <div class="img-data-item">
             <a href="#">
@@ -28,16 +41,18 @@ db.ref("popularinstamart/").get('value').then((snapshot) => {
             </div>
             </div>
             `;
-    });
-    mainData.appendChild(div);
+        mainData.appendChild(div);
 
 
-    const datas1 = document.getElementsByClassName('img-data-item');
+        const datas1 = document.getElementsByClassName('img-data-item');
 
-    for (var i = 0; i < datas1.length; i++) {
-        var button = datas1[i];
-        button.addEventListener('click', itemsClicked)
-    }
+        for (var i = 0; i < datas1.length; i++) {
+            var button = datas1[i];
+            button.addEventListener('click', itemsClicked)
+        }
+
+
+    })
 
 
 }).catch((err) => {
@@ -49,7 +64,13 @@ db.ref("popularinstamart/").get('value').then((snapshot) => {
 // second  
 
 db.ref("popularcuisines/").get('value').then((snapshot) => {
-    const data = snapshot.val()
+    const data = snapshot.val();
+
+
+
+
+
+    
     const mainData = document.getElementsByClassName('img-data')[1];
     const div = document.createElement('div');
     div.classList.add('img-data-div');
@@ -78,6 +99,9 @@ db.ref("popularcuisines/").get('value').then((snapshot) => {
     console.log(err.message);
 
 })
+
+
+
 
 
 
