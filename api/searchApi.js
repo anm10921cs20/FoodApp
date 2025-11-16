cfdfe8459371da3a073e29e8c8ad471ece9cfac7
mahavishnu <mahanvishnu3@gamil.com>
2025-11-17 00:17:43 +0530

    update

--- api/searchApi.js
+++ api/searchApi.js
@@ -1,23 +1,26 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAm--sfbEonD49xNHSWPWoeMbkwFXXqF4U",
+    authDomain: "mahan-food-app.firebaseapp.com",
+    projectId: "mahan-food-app",
+    storageBucket: "mahan-food-app.firebasestorage.app",
+    messagingSenderId: "33097873868",
+    appId: "1:33097873868:web:a34866029150b96d274569",
+    measurementId: "G-V1EX1JKP7S"
+};
 
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
 
+// first
 
-
-// api call
-const Apiurl = "https://users1-16jy.onrender.com/";
-async function fetchApi() {
-    
-} {
-    const loaderContent = document.getElementsByClassName('dots-container')[0];
-
-    loaderContent.classList.remove('loader-hidden')
-    try {
-        const responce = await fetch(`${Apiurl}popularinstamart`);
-        const data = await responce.json();
-        const mainData = document.getElementsByClassName('img-data')[0];
-        const div = document.createElement('div');
-        div.classList.add('img-data-div');
-        data.forEach((item) => {
-            div.innerHTML += `
+db.ref("popularinstamart/").get('value').then((snapshot) => {
+    const data = snapshot.val()
+    const mainData = document.getElementsByClassName('img-data')[0];
+    const div = document.createElement('div');
+    div.classList.add('img-data-div');
+    data.forEach((item) => {
+        div.innerHTML += `
             <div class="img-data-item">
             <a href="#">
             <div class="img-container">
@@ -25,72 +28,88 @@ async function fetchApi() {
             </div>
             </div>
             `;
-        });
-        mainData.appendChild(div);
+    });
+    mainData.appendChild(div);
 
 
-        const datas1 = document.getElementsByClassName('img-data-item');
+    const datas1 = document.getElementsByClassName('img-data-item');
 
-        for(var i = 0; i<datas1.length; i++)
-        {
-            var button = datas1[i];
-            button.addEventListener('click',itemsClicked)
-        }
-
-
+    for (var i = 0; i < datas1.length; i++) {
+        var button = datas1[i];
+        button.addEventListener('click', itemsClicked)
     }
-    catch (error) {
-        console.log(error.message)
-    }
-    finally
-    {
-        loaderContent.classList.add('loader-hidden')
-    }
-}
-fetchApi();
 
 
-// second call
+}).catch((err) => {
+    console.log(err.message);
 
-async function fetchApi1() {
-    
-}{
-    try {
-        const responce = await fetch(`${Apiurl}popularcuisines`);
-        const data = await responce.json();
-        const mainData = document.getElementsByClassName('img-data')[1];
-        const div = document.createElement('div');
-        div.classList.add('img-data-div');
-        data.forEach((item) => {
-            div.innerHTML += `
+})
+
+
+// second  
+
+db.ref("popularcuisines/").get('value').then((snapshot) => {
+    const data = snapshot.val()
+    const mainData = document.getElementsByClassName('img-data')[1];
+    const div = document.createElement('div');
+    div.classList.add('img-data-div');
+    data.forEach((item) => {
+        div.innerHTML += `
             <div class="img-data-item">
             <a href="#">
             <div class="img-container">
-            <img src="${item.img}" alt="instamartimg" class=""img-data-src width="110px" height="110px"/></a>
+            <img src="${item.img}" alt="instamartimg" class=""img-data-src width="100px" height="100px"/></a>
             </div>
             </div>
             `;
-        });
-
-        mainData.appendChild(div);
-
-
-        const datas1 = document.getElementsByClassName('img-data-item');
-
-        for(var i = 0; i<datas1.length; i++)
-        {
-            var button = datas1[i];
-            button.addEventListener('click',itemsClicked)
-        }
-        
+    });
+    mainData.appendChild(div);
 
 
+    const datas1 = document.getElementsByClassName('img-data-item');
+
+    for (var i = 0; i < datas1.length; i++) {
+        var button = datas1[i];
+        button.addEventListener('click', itemsClicked)
     }
-    catch (error) {
-        console.log(error.message)
-    }
-}
-fetchApi1();
+
+
+}).catch((err) => {
+    console.log(err.message);
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -101,23 +120,8 @@ fetchApi1();
 // functions place
 
 
-function itemsClicked(event)
-{
+function itemsClicked(event) {
     var btn = event.target;
-   console.log(btn.parentElement);
-   
-   
-   
-    
-    
-    
+    console.log(btn.parentElement);
 
-}
-export default function handler(req, res) {
-  res.status(200).json({
-    popularinstamart: [
-      { id: 1, name: "Vegetables", img: "https://..." },
-      { id: 2, name: "Cool Drinks", img: "https://..." }
-    ]
-  });
 }
