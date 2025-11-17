d9b4f285cf0fd03d6b04effffae15614fb411c4a
mahavishnu <mahanvishnu3@gamil.com>
2025-11-17 21:36:03 +0530

    update

--- src/search/searchfile.js
+++ src/search/searchfile.js
@@ -1,20 +1,202 @@
 
 const dataFilter = []
 
-db.ref('popularinstamart/').get('value').then((snapshot) => {
-    const dataInstamart = snapshot.val();
-    dataFilter.push(dataInstamart);
 
-}).catch(err => {
-    console.log(err.message);
-
-})
 
 
 db.ref('popularcuisines/').get('value').then((snapshot) => {
     const dataInstamart = snapshot.val();
     dataFilter.push(dataInstamart);
-    console.log(dataFilter.length);
+
+
+
+}).catch(err => {
+    console.log(err.message);
+
+})
+
+db.ref('Biriyani/').get('value').then((snapshot) => {
+    const dataInstamart = snapshot.val();
+    dataFilter.push(dataInstamart);
+
+
+
+
+}).catch(err => {
+    console.log(err.message);
+
+})
+db.ref('South Indian/').get('value').then((snapshot) => {
+    const dataInstamart = snapshot.val();
+    dataFilter.push(dataInstamart);
+
+
+
+
+}).catch(err => {
+    console.log(err.message);
+
+})
+
+db.ref('Pizza/').get('value').then((snapshot) => {
+    const dataInstamart = snapshot.val();
+    dataFilter.push(dataInstamart);
+
+
+
+
+}).catch(err => {
+    console.log(err.message);
+
+})
+
+db.ref('Chinese/').get('value').then((snapshot) => {
+    const dataInstamart = snapshot.val();
+    dataFilter.push(dataInstamart);
+
+
+
+
+}).catch(err => {
+    console.log(err.message);
+
+})
+
+db.ref('Cake/').get('value').then((snapshot) => {
+    const dataInstamart = snapshot.val();
+    dataFilter.push(dataInstamart);
+
+
+
+
+}).catch(err => {
+    console.log(err.message);
+
+})
+
+db.ref('Burger/').get('value').then((snapshot) => {
+    const dataInstamart = snapshot.val();
+    dataFilter.push(dataInstamart);
+
+
+
+
+}).catch(err => {
+    console.log(err.message);
+
+})
+
+db.ref('North Indian/').get('value').then((snapshot) => {
+    const dataInstamart = snapshot.val();
+    dataFilter.push(dataInstamart);
+
+
+
+
+}).catch(err => {
+    console.log(err.message);
+
+})
+
+db.ref('Rolls/').get('value').then((snapshot) => {
+    const dataInstamart = snapshot.val();
+    dataFilter.push(dataInstamart);
+
+
+
+
+}).catch(err => {
+    console.log(err.message);
+
+})
+
+db.ref('Shawarma/').get('value').then((snapshot) => {
+    const dataInstamart = snapshot.val();
+    dataFilter.push(dataInstamart);
+
+
+
+
+}).catch(err => {
+    console.log(err.message);
+
+})
+
+db.ref('Noodles/').get('value').then((snapshot) => {
+    const dataInstamart = snapshot.val();
+    dataFilter.push(dataInstamart);
+
+
+
+
+}).catch(err => {
+    console.log(err.message);
+
+})
+db.ref('Ice Cream/').get('value').then((snapshot) => {
+    const dataInstamart = snapshot.val();
+    dataFilter.push(dataInstamart);
+
+
+
+
+}).catch(err => {
+    console.log(err.message);
+
+})
+
+db.ref('Parotta/').get('value').then((snapshot) => {
+    const dataInstamart = snapshot.val();
+    dataFilter.push(dataInstamart);
+
+
+
+
+}).catch(err => {
+    console.log(err.message);
+
+})
+
+db.ref('Pasta/').get('value').then((snapshot) => {
+    const dataInstamart = snapshot.val();
+    dataFilter.push(dataInstamart);
+
+
+
+
+}).catch(err => {
+    console.log(err.message);
+
+})
+
+db.ref('Salad/').get('value').then((snapshot) => {
+    const dataInstamart = snapshot.val();
+    dataFilter.push(dataInstamart);
+
+
+
+
+}).catch(err => {
+    console.log(err.message);
+
+})
+
+db.ref('Paratha/').get('value').then((snapshot) => {
+    const dataInstamart = snapshot.val();
+    dataFilter.push(dataInstamart);
+
+
+
+
+}).catch(err => {
+    console.log(err.message);
+
+})
+
+db.ref('Pure Veg/').get('value').then((snapshot) => {
+    const dataInstamart = snapshot.val();
+    dataFilter.push(dataInstamart);
+
 
 
 
@@ -24,7 +206,58 @@ db.ref('popularcuisines/').get('value').then((snapshot) => {
 })
 
 
-console.log(dataFilter.length);
+db.ref('Shake/').get('value').then((snapshot) => {
+    const dataInstamart = snapshot.val();
+    dataFilter.push(dataInstamart);
+
+
+
+
+}).catch(err => {
+    console.log(err.message);
+
+})
+
+
+db.ref('Dosa/').get('value').then((snapshot) => {
+    const dataInstamart = snapshot.val();
+    dataFilter.push(dataInstamart);
+
+
+
+
+}).catch(err => {
+    console.log(err.message);
+
+})
+
+
+db.ref('Pancake/').get('value').then((snapshot) => {
+    const dataInstamart = snapshot.val();
+    dataFilter.push(dataInstamart);
+
+
+
+
+}).catch(err => {
+    console.log(err.message);
+
+})
+
+
+db.ref('Vada/').get('value').then((snapshot) => {
+    const dataInstamart = snapshot.val();
+    dataFilter.push(dataInstamart);
+
+
+
+
+}).catch(err => {
+    console.log(err.message);
+
+})
+
+
 
 
 
@@ -55,12 +288,11 @@ input.addEventListener('input', function (e) {
         }
     }
 
-    console.log(matches);
 
     // optional: render results if an element with id="results" exists
     const resultsEl = document.getElementsByClassName('results')[0];
     if (resultsEl) {
-        resultsEl.innerHTML = matches.map(it => `<li class="list-content"><a href=""><div class="img-content"><img src="${it.img}" width="80px" height="90px"></div> <div class="name-content"><p class="pr-name">${it.name}</p><p class="pr-rel">${it.rel}</p></div> </a> </li>`).join('');
+        resultsEl.innerHTML = matches.map(it => `<li class="list-content"><a href=""><div class="img-content"><img src="${it.img}" class="img-ctrl"></div> <div class="name-content"><p class="pr-name">${it.name}</p><p class="pr-rel">${it.rel}</p></div> </a> </li>`).join('');
 
     }
 });
