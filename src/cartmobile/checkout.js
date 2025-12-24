a3b2de43ef7237719d6560a2b80deaf43cfb1210
mahavishnu <mahanvishnu3@gamil.com>
2025-12-24 23:21:33 +0530

    update

--- src/cartmobile/checkout.js
+++ src/cartmobile/checkout.js
@@ -195,7 +195,7 @@ db.ref("MahanFoodCart/" + name + uid).get('value').then((snapshot) => {
             const responce = snapshot.val()
 
             const productPrices = document.getElementsByClassName('product-plus-store');
-             
+
 
             const quantityInputAll = document.getElementsByClassName('quantity-input');
             const quantityvalue = responce.quantity;
@@ -225,3 +225,246 @@ db.ref("MahanFoodCart/" + name + uid).get('value').then((snapshot) => {
 })
 
 
+
+
+
+
+
+
+
+
+
+// location access
+
+function Access() {
+    const blurContainer = document.getElementsByClassName('checkout-location-access-blur-container')[0];
+    const accessContainer = document.getElementsByClassName('checkout-location-access-container')[0]
+
+
+    blurContainer.classList.add('checkout-location-access-blur-container-active');
+    accessContainer.classList.add('checkout-location-access-container-active');
+
+}
+
+// location access close
+
+const locationAccessClose = document.getElementById('location-access-close');
+
+locationAccessClose.addEventListener('click', () => {
+    const blurContainer = document.getElementsByClassName('checkout-location-access-blur-container')[0];
+    const accessContainer = document.getElementsByClassName('checkout-location-access-container')[0]
+
+    blurContainer.classList.remove('checkout-location-access-blur-container-active');
+    accessContainer.classList.remove('checkout-location-access-container-active');
+})
+
+const blurContainer = document.getElementsByClassName('checkout-location-access-blur-container')[0];
+blurContainer.addEventListener('click', () => {
+    const blurContainer = document.getElementsByClassName('checkout-location-access-blur-container')[0];
+    const accessContainer = document.getElementsByClassName('checkout-location-access-container')[0]
+
+    blurContainer.classList.remove('checkout-location-access-blur-container-active');
+    accessContainer.classList.remove('checkout-location-access-container-active');
+})
+
+
+//   location edit container
+
+const locationEdit = document.getElementById('checkout-address-edit-close');
+
+locationEdit.addEventListener('click', () => {
+    const checkoutAddressEditContainer = document.getElementsByClassName('checkout-address-edit-container')[0]
+    checkoutAddressEditContainer.style.display = "none";
+})
+
+
+// location edit container open
+
+const editBtn = document.getElementsByClassName('Edit')[0];
+
+editBtn.addEventListener('click', () => {
+    const editContainerOpen = document.getElementsByClassName('checkout-address-edit-container')[0];
+    editContainerOpen.style.display = "block";
+})
+
+
+
+// address details upload
+
+
+function AddressData() {
+    const street = document.getElementById('street').value;
+    const district = document.getElementById('district').value;
+    const state = document.getElementById('state').value;
+    const pincode = document.getElementById('pincode').value;
+    const phonenumber = document.getElementById('phonenumber').value;
+    localStorage.setItem('phonenumbermob', phonenumber)
+    const input = document.getElementsByTagName('input');
+
+    if (!pincode) {
+        for (var i = 0; i < input.length; i++) {
+            if (street == "") {
+                input[i].style.border = "1px solid #ff4343";
+
+            }
+            else if (district == "") {
+                input[i].style.border = "1px solid #ff4343";
+
+            }
+            else if (state == "") {
+                input[i].style.border = "1px solid #ff4343";
+
+            }
+            else if (pincode == "") {
+                input[i].style.border = "1px solid #ff4343";
+                
+
+            }
+
+            setTimeout(() => {
+                for (var i = 0; i < input.length; i++) {
+                    input[i].style.border = "1px solid rgba(0,0,0,0.2)";
+                }
+
+            }, 3000)
+
+
+        }
+
+    }
+    else {
+
+        const alertText = document.getElementsByClassName('notification__text')[0];
+        const alertbox = document.getElementsByClassName('alert-container-true')[0];
+        alertbox.style.display = "block";
+        alertText.innerText = "Edit Successfully";
+        setTimeout(function () {
+            alertbox.style.display = "none";
+            window.location.reload();
+        }, 4000)
+        const uiddata = localStorage.getItem('uid');
+        firestore.collection('UserAddressMobile/').doc(uiddata).set(
+            {
+                street: street,
+                district: district,
+                state: state,
+                pincode: pincode
+            }
+        )
+    }
+
+}
+
+const uiddata = localStorage.getItem('uid');
+firestore.collection('UserAddressMobile/').doc(uiddata).get().then((responce) => {
+    const dataresponce = responce.data();
+
+
+    if (dataresponce) {
+
+
+        const addressdet = dataresponce.street + " " + dataresponce.district + " " + dataresponce.state + " " + dataresponce.pincode ?? 0;
+        const addressDet = document.getElementsByClassName('AddressData')[0];
+
+        addressDet.innerText = addressdet ?? "Please Edit Address";
+
+        const phone = localStorage.getItem('phonenumbermob');
+
+        const phoneElement = document.getElementsByClassName('MobileData')[0];
+        phoneElement.innerText = phone ?? "Please Edit Phonenumber"
+
+
+
+
+
+    }
+})
+
+
+function ApiCall() {
+
+
+    navigator.geolocation.getCurrentPosition(async (position) => {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        const addressData = await getAddressFromLatLng(lat, lon);
+        console.log(addressData);
+
+
+        const street = document.getElementById('street');
+        const district = document.getElementById('district');
+        const state = document.getElementById('state');
+        const pincode = document.getElementById('pincode');
+        const street1 = document.getElementById('streetid');
+        const district1 = document.getElementById('districtid');
+        const state1 = document.getElementById('stateid');
+        const pincode1 = document.getElementById('pincodeid');
+        street.value = addressData.fullAddress;
+        district.value = addressData.district;
+        state.value = addressData.state;
+        pincode.value = addressData.pincode;
+
+
+
+
+
+        street1.value = addressData.fullAddress;
+        district1.value = addressData.district;
+        state1.value = addressData.state;
+        pincode1.value = addressData.pincode;
+
+        // state.value = data.results[0].state;
+        // pincode.value = data.results[0].postcode;
+        // street1.value = data.results[0].street;
+        // district1.value = data.results[0].city;
+        // state1.value = data.results[0].state;
+        // pincode1.value = data.results[0].postcode;
+
+
+
+
+        async function getAddressFromLatLng(lat, lon) {
+            const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}&addressdetails=1`;
+
+            const response = await fetch(url, {
+                headers: {
+                    "User-Agent": "your-app-name" // required
+                }
+            });
+
+            const data = await response.json();
+
+            if (data) {
+                const alertText = document.getElementsByClassName('notification__text')[0];
+                const alertbox = document.getElementsByClassName('alert-container-true')[0];
+                alertbox.style.display = "block";
+                alertText.innerText = "Loacation Get Successfully";
+                setTimeout(function () {
+                    alertbox.style.display = "none";
+                    const blurContainer = document.getElementsByClassName('checkout-location-access-blur-container')[0];
+                    const accessContainer = document.getElementsByClassName('checkout-location-access-container')[0]
+
+                    blurContainer.classList.remove('checkout-location-access-blur-container-active');
+                    accessContainer.classList.remove('checkout-location-access-container-active');
+
+                }, 2000)
+            }
+            const address = data.address;
+            console.log(data);
+
+
+            return {
+                fullAddress: data.name,
+                district: address.county || address.state_district,
+                state: address.state,
+                pincode: address.postcode,
+                country: address.country
+            };
+        }
+
+
+
+
+
+    })
+}
